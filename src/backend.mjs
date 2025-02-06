201122b21b005d7ddcee6cc02de3f7309f011060
noahrognon <noah.rognon@edu.univ-fcomte.fr>
2025-02-06 15:34:50 +0100

    tp  3 oublie de commit

---
+++ src/backend.mjs
@@ -0,0 +1,21 @@
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+export async function getOffres() {
+    try {
+        let data = await pb.collection('Maison').getFullList({
+            sort: '-created',
+        });
+
+        data = data.map((maison) => {
+            maison.imageUrl = pb.files.getURL(maison, maison.image);
+            return maison;
+        });
+
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
+}
