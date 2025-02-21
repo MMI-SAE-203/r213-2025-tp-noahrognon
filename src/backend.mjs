b6104028f90145adc63b0e9b8af5a099c1b6e215
noahrognon <noah.rognon@edu.univ-fcomte.fr>
2025-02-21 09:33:26 +0100

    fin de tp 6

--- src/backend.mjs
+++ src/backend.mjs
@@ -4,7 +4,7 @@ const pb = new PocketBase('http://127.0.0.1:8090');
 
 export async function getOffres() {
     try {
-        let data = await pb.collection('Maison').getFullList({
+        let data = await pb.collection('maison').getFullList({
             sort: '-created',
         });
 
@@ -24,23 +24,25 @@ export async function getOffre(id) {
     try {
         let data = await pb.collection('maison').getOne(id);
         data.imageUrl = pb.files.getURL(data, data.image);
-        console.log("data",data);
-        
+        console.log("data", data);
+
         return data;
     } catch (error) {
         console.log('Une erreur est survenue en lisant la maison', error);
         return null;
     }
 }
+
 export async function getMaisons() {
     try {
-        let data = await pb.collection('maison').getFullList(); // Vérifie que 'maison' est bien le bon nom de collection
+        let data = await pb.collection('maison').getFullList();
         return data;
     } catch (error) {
         console.error("Erreur lors de la récupération des maisons :", error);
         return [];
     }
 }
+
 export async function addOffre(house) {
     try {
         await pb.collection('maison').create(house);
@@ -56,6 +58,7 @@ export async function addOffre(house) {
         };
     }
 }
+
 export async function filterByPrix(prixMin, prixMax) {
     try {
         let data = await pb.collection('maison').getFullList({
@@ -69,6 +72,47 @@ export async function filterByPrix(prixMin, prixMax) {
         return data;
     } catch (error) {
         console.log('Une erreur est survenue en filtrant la liste des maisons', error);
+        return [];
+    }
+}
+
+export async function allAgents() {
+    try {
+        let data = await pb.collection("agent").getFullList();
+        data = data.map((agent) => {
+            agent.imageUrl = pb.files.getURL(agent, agent.image);
+            return agent;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des agents', error);
+        return [];
+    }
+}
+
+export async function getAgent(id) {
+    try {
+        let data = await pb.collection("agent").getOne(id);
+        data.imageUrl = pb.files.getURL(data, data.image);
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant l\'agent', error);
+        return null;
+    }
+}
+export async function setFavori(house) {
+    await pb.collection('maison').update(house.id, { favori: !house.favori });
+}
+export async function getFavoris() {
+    try {
+        let data = await pb.collection('maison').getFullList({ filter: "favori = true" });
+        data = data.map((maison) => {
+            maison.imageUrl = pb.files.getURL(maison, maison.image);
+            return maison;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des favoris', error);
         return [];
     }
 }
